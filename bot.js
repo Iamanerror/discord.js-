7d17e65f8956f31443e51ad2a7dcf7b22a50a016
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2019-03-12 17:35:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const { Discord, Client, RichEmbed, Attachment } = require("discord.js");
 const client = new Client({disableEveryone: true});
+const Canvas = require('canvas');
+const snekfetch = require('snekfetch');
+
+
 
 client.on("ready", () => {
 
@@ -8,15 +12,39 @@ client.on("ready", () => {
   
 });
 
-client.on('guildMemberAdd', member => {
-    let embed = new Discord.RichEmbed()
-   .setTitle("Member Joined!")
-   .addField("Name:", member.user)
-   .addField("User ID:", member.id)
-   .addField("Member Count:", message.memberCount)
-   .setColor(0x0463ff)
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+	if (!channel) return;
 
-  member.guild.channels.get('535002095460417561').send(embed); 
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+	const avatar = await Canvas.loadImage(buffer);
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 client.on("guildMemberRemove", member => {
